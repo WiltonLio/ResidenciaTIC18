cd664ba61647e35d4173378d2334a88f67079a76
WiltonLio <wilton.lio@gmail.com>
2024-02-22 16:35:24 -0300

    organizando e enviando as atividades

---
+++ 3. Trilha-DotNet/Aula13/MiddleWare/Program.cs
@@ -0,0 +1,15 @@
+namespace MiddleWare;
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddScoped<LinhaDeMontagemDescricao>();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+
+// app.UseMiddleware<AddChassiMiddleware>();
+// app.UseMiddleware<AddMotorMiddleware>();
+// app.UseMiddleware<AddPortasMiddleware>();
+// app.UseMiddleware<AddPinturaMiddleware>();
+// app.UseMiddleware<AddInternoMiddleware>();
+
+app.Run();
